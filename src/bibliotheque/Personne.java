3e35321f524e5d20478eaaa3438c1ac31a572fe0
valentin <valentin@valentin-Aspire-VN7-571G>
2016-05-17 08:45:17 +0200

    Premier Upload des classes

---
+++ src/bibliotheque/Personne.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibliotheque;
+
+/**
+ *
+ * @author valentin
+ */
+public class Personne {
+    private int numeroPers;
+    private String nomPers;
+    private String prenomPers;
+    private int anNaissance;
+    private static int dernierNumero = 1;
+    
+    /**
+     * Constructeur
+     * @param nom : nom de la personne
+     * @param prenom : prénom de la personne
+     * @param anneenaiss : année de naissance de la personne
+     */
+    public Personne(String nom,String prenom,int anneenaiss){
+        
+    }
+    
+    /**
+     * Obtention numéro de la personne
+     * @return NumeroPersonne
+     */
+    public int getNumero(){
+        return 0;
+    }
+    
+    /**
+     * Obtention nom de la personne
+     * @return NomPersonne
+     */
+    public String getNom(){
+        return "";
+    }
+    
+    /**
+     * Obtention Prénom de la Personne
+     * @return PrenomPersonne
+     */
+    public String getPrenom(){
+        return "";
+    }
+    
+    /**
+     * Obtention Annéee de naissance de la personne
+     * @return AnneeNaissance
+     */
+    public int getAnNaissance(){
+        return 0;
+    }
+    
+    /**
+     * Obtention Dernier numéro
+     * @return DernierNum
+     */
+    public int getDernierNum(){
+        return 0;
+    }
+    
+    /**
+     * Changement variable Numéro Personne
+     * @param numero : numeroPersonne
+     */
+    public void setNumPers(int numero){
+        
+    }
+    
+    /**
+     * Changement variable Nom de la Personne
+     * @param nom : nomPersonne
+     */
+    public void setNomPers(String nom){
+        
+    }
+    
+    /**
+     * Changement variable Prénom de la Personne
+     * @param prenom : prenomPersonne
+     */
+    public void setPrenomPers(String prenom){
+        
+    }
+    
+    /**
+     * Changement variable Année de naissance de la Personne
+     * @param annee : AnNaissance
+     */
+    public void setAnNaissance(int annee){
+        
+    }
+    
+    /**
+     * Redéfinition de toString()
+     * @return chaîne
+     */
+    public String toString(){
+        return "";
+    }
+    
+}
